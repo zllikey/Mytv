c269fc29d2bb1b37844d8f18a633a501ba59f5c0
zllikey <zllikey@gmail.com>
2014-07-12 18:35:04 +0800

    Create 奉上我的一气呵成回编classex到apk的bat文件.bat
    1.必须安装java，并且系统环境必须包含java所在目录
    2.set NAME=mytv  mytv为apk名字
    3.可选自动连接模拟器和安装apk文件，只能覆盖安装，暂时没有提供卸载安装
      adb connect 127.0.0.1:5551 为连接模拟器
      install -r Signed_*.apk 为安装apk
    4.执行后如果没有出错，则会生产Signed_mytv.apk文件，该文件是签名后的文件，可以直接安装,还可做小改动取消签名步骤

---
+++ 奉上我的一气呵成回编classex到apk的bat文件.bat
@@ -0,0 +1,44 @@
+@echo off
+
+set NAME=mytv
+
+set filename=%NAME%.apk
+set time1=%time:~0,2%%time:~3,2%%time:~6,2%
+
+
+
+cd /d %~dp0
+del /Q /F /S classes.dex *signed.apk *_classes.dex Signed_*>nul 2>NUL
+
+echo 正在回编DEX
+rem java -jar "smali.jar" Smali* -o "%NAME%_classes.dex">NUL 2>NUL
+java -jar "smali.jar" Smali* -o "classes.dex">NUL 2>NUL
+
+if exist "classes.dex" (
+echo 回编完成
+rem ren *_classes.dex classes.dex
+
+rem copy Smali_mytvhd_classes.dex classes.dex
+echo 添加DEX到%filename%
+rem "C:\Program Files\7-Zip\7z.exe" a -tzip -mx9 %filename% classes.dex>NUL 2>NUL
+7z.exe a -tzip %filename% classes.dex>NUL 2>NUL
+echo 签名%filename%
+java -jar "signapk.jar" "testkey.x509.pem" "testkey.pk8" %filename% "Signed_%filename%" >nul 2>NUL
+rem echo 安装%filename%
+rem adb connect 127.0.0.1:5551
+rem adb install -r Signed_*.apk
+rem echo 安装完毕
+
+
+) else (
+echo 编译失败
+
+)
+
+
+
+set time2=%time:~0,2%%time:~3,2%%time:~6,2%
+set /a time3=%time2%-%time1%
+echo 用时%time3%秒
+ping 127.1 -n 3 >NUL 2>NUL
+exit
